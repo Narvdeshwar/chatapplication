7345924a8d878338af8683ff4c3ec5901cc1ca54
narvdeshwar <eternal02.oct@gmail.com>
2024-12-26 00:12:44 +0530

    user info completed

---
+++ frontend/src/components/Right.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Right() {
+  return (
+    <div className='flex grow bg-slate-200'>
+        {/* user info */}
+        <div></div>
+    </div>
+  )
+}
+
+export default Right
