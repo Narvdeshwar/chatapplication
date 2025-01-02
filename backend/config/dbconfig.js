8443360df56523d02f7d48fa8a727f231677dd46
narvdeshwar <eternal02.oct@gmail.com>
2025-01-02 06:51:14 +0530

    backend connection

---
+++ backend/config/dbconfig.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const DB_CONNECT = async () => {
+  const MONGODB_URL = process.env.MONGODB_URI;
+  try { 
+    const connect = await mongoose.connect(MONGODB_URL);
+    console.log("MongoDB connected successfully:", connect);
+  } catch (err) {
+    console.error("Error while connecting to the DB:", err.message);
+    process.exit(1); // Exit process on failure
+  }
+};
+
+export default DB_CONNECT; // Default export for easier imports
